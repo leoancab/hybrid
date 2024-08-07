4d4f04af05bb9bdec549c7faf8d064d033334cd2
leoancab <leoancab@espol.edu.ec>
2024-08-07 10:43:22 -0500

    guía 25 stop 2

--- src/app/services/photo.service.ts
+++ src/app/services/photo.service.ts
@@ -1,23 +1,72 @@
 import { Injectable } from '@angular/core';
 import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
 import { Filesystem, Directory } from '@capacitor/filesystem';
+import { Platform } from '@ionic/angular';
 import { UserPhoto } from '../interfaces/user-photo';
+import { Capacitor } from '@capacitor/core';
 
 @Injectable({
   providedIn: 'root'
 })
 export class PhotoService {
   public photos: UserPhoto[] = [];
-  constructor() { }
+  private platform: Platform;
+  constructor(platform: Platform) {
+    this.platform = platform;
+  }
   public async addNewToGallery() {
     const capturedPhoto = await Camera.getPhoto({
       resultType: CameraResultType.Uri,
       source: CameraSource.Camera,
       quality: 100
     });
+    const savedImageFile = await this.savePicture(capturedPhoto);
+    this.photos.unshift(savedImageFile);
+    /*
     this.photos.unshift({
       filepath: "soon...",
       webviewPath: capturedPhoto.webPath!
-    });
+    });*/
   }
+  private async savePicture(photo: Photo) {
+    const base64Data = await this.readAsBase64(photo);
+    const hoy = new Date();
+    const fileName = hoy.getFullYear().toString() + (hoy.getMonth() + 1).toString() + hoy.getDate().toString() + hoy.getHours().toString().toString() + hoy.getMinutes().toString() + hoy.getSeconds().toString() + '.jpeg';
+    const savedFile = await Filesystem.writeFile({
+      path: fileName,
+      data: base64Data,
+      directory: Directory.Data
+    });
+    if (this.platform.is('hybrid')) {
+      return {
+        filepath: savedFile.uri,
+        webviewPath: Capacitor.convertFileSrc(savedFile.uri),
+      };
+    } else {
+      return {
+        filepath: fileName,
+        webviewPath: photo.webPath
+      };
+    }
+  }
+  private async readAsBase64(photo: Photo) {
+    if (this.platform.is('hybrid')) {
+      const file = await Filesystem.readFile({
+        path: photo.path!
+      });
+      return file.data;
+    } else {
+      const response = await fetch(photo.webPath!);
+      const blob = await response.blob();
+      return await this.convertBlobToBase64(blob) as string;
+    }
+  }
+  private convertBlobToBase64 = (blob: Blob) => new Promise((resolve, reject) => {
+    const reader = new FileReader();
+    reader.onerror = reject;
+    reader.onload = () => {
+      resolve(reader.result);
+    };
+    reader.readAsDataURL(blob);
+  });
 }
